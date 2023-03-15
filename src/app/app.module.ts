1432f79fffd3614bacc7de617e4bb70b598f999c
Nihad KV <nihad083@gmail.com>
2023-03-16 00:22:58 +0530

    table of content implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableOfContentsComponent } from './table-of-contents/table-of-contents.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableOfContentsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/table-of-contents/table-of-contents.component.ts
@@ -0,0 +1,57 @@
+import {
+  Component,
+  OnInit,
+  QueryList,
+  Renderer2,
+  ViewChildren,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-table-of-contents',
+  templateUrl: './table-of-contents.component.html',
+  styleUrls: ['./table-of-contents.component.scss'],
+})
+export class TableOfContentsComponent implements OnInit {
+  @ViewChildren('overview, section1, section2, section3, section4, section5')
+  elms!: QueryList<any>;
+  detectedElms: any = [];
+  contents = [
+    'overview',
+    'section1',
+    'section2',
+    'section3',
+    'section4',
+    'section5',
+  ];
+  detectElms() {
+    const detectedElms: any = [];
+    this.elms.forEach((elm, index) => {
+      if (isInViewport(elm.nativeElement)) {
+        detectedElms.push(elm.nativeElement.id);
+        this.detectedElms = detectedElms;
+      }
+    });
+    console.log('dd', this.detectedElms);
+  }
+  constructor(private renderer: Renderer2) {
+    this.renderer.listen('window', 'resize', this.detectElms.bind(this));
+    this.renderer.listen('window', 'scroll', this.detectElms.bind(this));
+  }
+
+  ngAfterViewInit() {
+    setTimeout(this.detectElms.bind(this));
+  }
+
+  ngOnInit(): void {}
+}
+
+function isInViewport(elm: any) {
+  var elementTop = elm.offsetTop;
+  var elementBottom = elementTop + elm.offsetHeight;
+
+  // in this specific case the scroller is document.documentElement (<html></html> node)
+  var viewportTop = document.documentElement.scrollTop;
+  var viewportBottom = viewportTop + document.documentElement.clientHeight;
+
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+}
